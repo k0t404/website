3662451c0519968f9529a56dc93533f46e5192fc
Egor404 <eggr.5877@gmail.com>
2023-04-18 20:52:49 +0300

    little site development

--- main.py
+++ main.py
@@ -46,6 +46,11 @@ def index():
     return render_template("index.html", items=items)
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/account")
 def account():
     db_sess = db_session.create_session()
